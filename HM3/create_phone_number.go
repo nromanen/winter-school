27f8db9576991c9f8f7d55be2dfba30001b43971
Maikl-k <87263615+Maikl-k@users.noreply.github.com>
2023-02-22 12:43:23 +0200

    HM3

---
+++ HM3/create_phone_number.go
@@ -0,0 +1,11 @@
+package kata
+//https://www.codewars.com/kata/525f50e3b73515a6db000b83
+import "fmt"
+
+func CreatePhoneNumber(numbers [10]uint) string {
+	s := ""
+	for _, n := range numbers {
+		s += fmt.Sprintf("%v", n)
+	}
+	return fmt.Sprintf("(%v) %v-%v", s[0:3], s[3:6], s[6:10])
+}

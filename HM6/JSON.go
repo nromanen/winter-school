df82969736a7ce509a679ca552844073dafa50af
SashaVUa <34603035+SashaVUa@users.noreply.github.com>
2023-02-28 16:23:47 +0200

    6

--- HM6/JSON.go
+++ HM6/JSON.go
@@ -13,6 +13,6 @@ func isJSON(s string) bool {
 }
 
 func main() {
-	fmt.Println(checkJSON("{\n  \"firstName\": \"John\",\n  \"lastName\": \"Smith\",\n  \"isAlive\": true,\n  \"age\": 27,\n  \"address\": {\n    \"streetAddress\": \"21 2nd Street\",\n    \"city\": \"New York\",\n    \"state\": \"NY\",\n    \"postalCode\": \"10021-3100\"\n  },\n  \"phoneNumbers\": [\n    {\n      \"type\": \"home\",\n      \"number\": \"212 555-1234\"\n    },\n    {\n      \"type\": \"office\",\n      \"number\": \"646 555-4567\"\n    }\n  ],\n  \"children\": [\n      \"Catherine\",\n      \"Thomas\",\n      \"Trevor\"\n  ],\n  \"spouse\": null\n}"))
-	fmt.Println(checkJSON("{[\"object\": \"string\""))
+	fmt.Println(isJSON("{\n  \"firstName\": \"Ok\",\n  \"lastName\": \"Trni\",\n  \"isAlive\": true,\n  \"age\": 15,\n  \"address\": {\n    \"streetAddress\": \"21 2nd Street\",\n    \"city\": \"New York\",\n    \"state\": \"NY\",\n    \"postalCode\": \"10021-3100\"\n  },\n  \"phoneNumbers\": [\n    {\n      \"type\": \"home\",\n      \"number\": \"212 555-1234\"\n    },\n    {\n      \"type\": \"office\",\n      \"number\": \"646 555-4567\"\n    }\n  ],\n  \"children\": [\n      \"Catherine\",\n      \"Thomas\",\n      \"Trevor\"\n  ],\n  \"spouse\": null\n}"))
+	fmt.Println(isJSON("{[\"object\": \"string\""))
 }
